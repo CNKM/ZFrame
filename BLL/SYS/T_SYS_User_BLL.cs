c5b30e32c65f8ac9465e5a614511d8b8262b9784
PeerLessSoul <km.liuz@Gmail.com>
2015-01-07 12:27:25 +0800

    调整登陆验证存储过程

--- BLL/SYS/T_SYS_User_BLL.cs
+++ BLL/SYS/T_SYS_User_BLL.cs
@@ -26,56 +26,53 @@ namespace BLL.SYS
         /// <param name="tUserName">用户名称</param>
         /// <param name="tUserPWD">用户密码</param>
         /// <returns>1 通过验证 -1 未通过验证</returns>
-        //public String CheckUserLogin(
-        //    string tUserName,
-        //    string tUserPWD,
-        //    out T_SYS_ORG_Dept t_SYS_ORG_Dept,
-        //    out T_SYS_ORG_Position t_SYS_ORG_Position,
-        //    out T_SYS_ORG_Person t_SYS_ORG_Person,
-        //    out List<T_SYS_Func_Resources> UserFuncs,
-        //    out List<T_SYS_ORG_Position> ChoosePositionSource,
-        //    String ChooseDeptid = "")
-        //{
-        //    Int32 ExecResult = 0;
-        //    List<SqlParameter> Parms = new List<SqlParameter>();
-        //    Parms.Add(new SqlParameter() { ParameterName = "@LoginName", Value = tUserName });
-        //    Parms.Add(new SqlParameter() { ParameterName = "@LoginPWD", Value = tUserPWD });
-        //    Parms.Add(new SqlParameter() { ParameterName = "@LoginPosition", Value = ChooseDeptid });
-        //    Parms.Add(new SqlParameter() { ParameterName = "@LoginResult", DbType = System.Data.DbType.String, Size = 60, Direction = ParameterDirection.Output });
-        //    DataSet returnDs = this.ExecProceureRetrunList("USP_CheckUserLogin", Parms, out ExecResult, null, false);
-        //    String ExecResultString = Parms[3].Value.ToString();
-
-
-        //    t_SYS_ORG_Dept = null;
-        //    t_SYS_ORG_Person = null;
-        //    t_SYS_ORG_Position = null;
-        //    UserFuncs = null;
-        //    ChoosePositionSource = null;
-
-        //    if (String.IsNullOrEmpty(ExecResultString))
-        //    {
-        //        if (returnDs.Tables.Count == 4)
-        //        {
-        //            t_SYS_ORG_Person = this.ToList<T_SYS_ORG_Person>(returnDs.Tables[0])[0];
-        //            t_SYS_ORG_Dept = this.ToList<T_SYS_ORG_Dept>(returnDs.Tables[1])[0];
-        //            t_SYS_ORG_Position = this.ToList<T_SYS_ORG_Position>(returnDs.Tables[2])[0];
-        //            UserFuncs = this.ToList<T_SYS_Func_Resources>(returnDs.Tables[3]);
-        //            ChoosePositionSource = null;
-        //        }
-        //        return "";
-        //    }
-        //    else
-        //    {
-
-        //        ChoosePositionSource = returnDs.Tables.Count == 2 ? this.ToList<T_SYS_ORG_Position>(returnDs.Tables[1]) : null;
-        //        return ExecResultString;
-        //    }
-        //}
-
-
-        public void UserCheck(string UserName, string PassWord)
+        public String CheckUserLogin(
+            string tUserName,
+            string tUserPWD,
+            out T_SYS_Dept t_SYS_ORG_Dept,
+            out T_SYS_Role t_SYS_ORG_Position,
+            out T_SYS_UserInfo t_SYS_ORG_Person,
+            out List<T_SYS_FunctionRight> UserFuncs,
+            out List<T_SYS_Role> ChoosePositionSource,
+            String ChooseDeptid = "")
         {
+            Int32 ExecResult = 0;
+            List<SqlParameter> Parms = new List<SqlParameter>();
+            Parms.Add(new SqlParameter() { ParameterName = "@LoginName", Value = tUserName });
+            Parms.Add(new SqlParameter() { ParameterName = "@LoginPWD", Value = tUserPWD });
+            Parms.Add(new SqlParameter() { ParameterName = "@LoginPosition", Value = ChooseDeptid });
+            Parms.Add(new SqlParameter() { ParameterName = "@LoginResult", DbType = System.Data.DbType.String, Size = 60, Direction = ParameterDirection.Output });
+            DataSet returnDs = this.ExecProceureRetrunList("USP_CheckUserLogin", Parms, out ExecResult, null, false);
+            String ExecResultString = Parms[3].Value.ToString();
 
+
+            t_SYS_ORG_Dept = null;
+            t_SYS_ORG_Person = null;
+            t_SYS_ORG_Position = null;
+            UserFuncs = null;
+            ChoosePositionSource = null;
+
+            if (String.IsNullOrEmpty(ExecResultString))
+            {
+                if (returnDs.Tables.Count == 4)
+                {
+                    t_SYS_ORG_Person = this.ToList<T_SYS_UserInfo>(returnDs.Tables[0])[0];
+                    t_SYS_ORG_Dept = this.ToList<T_SYS_Dept>(returnDs.Tables[1])[0];
+                    t_SYS_ORG_Position = this.ToList<T_SYS_Role>(returnDs.Tables[2])[0];
+                    UserFuncs = this.ToList<T_SYS_FunctionRight>(returnDs.Tables[3]);
+                    ChoosePositionSource = null;
+                }
+                return "";
+            }
+            else
+            {
+
+                ChoosePositionSource = returnDs.Tables.Count == 2 ? this.ToList<T_SYS_Role>(returnDs.Tables[1]) : null;
+                return ExecResultString;
+            }
         }
+
+
+
     }
 }
--- ZFrameWCF/WCFServices.svc.cs
+++ ZFrameWCF/WCFServices.svc.cs
@@ -47,7 +47,12 @@ namespace ZFrameWCF
         public Stream Login_UserCheck(String UserName, String PassWord, String CheckCode)
         {
             T_SYS_User_BLL UserBLL = new T_SYS_User_BLL();
-            UserBLL.UserCheck(UserName, PassWord);
+            T_SYS_Dept t_SYS_ORG_Dept;
+            T_SYS_Role t_SYS_ORG_Position;
+            T_SYS_UserInfo t_SYS_ORG_Person;
+            List<T_SYS_FunctionRight> UserFuncs;
+            List<T_SYS_Role> ChoosePositionSource;
+            UserBLL.CheckUserLogin(UserName, PassWord, out t_SYS_ORG_Dept, out t_SYS_ORG_Position, out t_SYS_ORG_Person, out UserFuncs, out ChoosePositionSource);
             return UserName.ToStream();
         }
 
