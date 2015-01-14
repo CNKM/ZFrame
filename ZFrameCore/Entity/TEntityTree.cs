144eacd6a504ecc4c397e816c1f5f3775a638076
PeerLessSoul <km.liuz@Gmail.com>
2015-01-14 14:16:49 +0800

    Add TEntityTree

---
+++ ZFrameCore/Entity/TEntityTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZFrameCore.Common;
+
+namespace ZFrameCore.Entity
+{
+    public class TEntityTree<T>:TEntityBase where T:TEntityBase
+    {
+        T Parent;
+        List<T> _Children;
+        [PropertyType(EntityPropertyType.DesignField)]
+        public List<T> Children
+        {
+            get { return _Children; }
+            set { _Children = value; }
+
+        }
+        public TEntityTree()
+        {
+            this._Children = new List<T>();
+        }
+
+
+    
+    }
+}
---
+++ ZFrameWCF/WCF_Func/WCF_Function.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Comm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+using ZFrameCore.Common;
+namespace ZFrameWCF
+{
+    public partial class WCFServices
+    {
+       
+    }
+}
